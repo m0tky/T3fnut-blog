4e87e2e3df4b14c22df5812198823451de19c157
m0tky <43568769+m0tky@users.noreply.github.com>
2020-05-27 22:18:08 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/m0tky/c28d018d497a4e2e8683403f54eb43e9?v=6439b17f87784027a99259907d36475a"
+                configTable: "https://www.notion.so/m0tky/cedf19069392486c843f571139212797?v=34387e9287ef45559aa8f8865d586ba3"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/m0tky/616b8d8006a94e56b57214e51dcc1a86?v=affc85534a6343bab8b262e31b82f91d"
+                configTable: "https://www.notion.so/m0tky/84f465ca3cb2407f9054e07ff330ef6a?v=adf4984212444b1381ef2dddd96326c4"
             }
         }
     ],
